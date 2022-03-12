3bc8e2bc8db58f0658039df4c1e6ebfb0a92ea9b
JintaoYang18 <colsonyang212@gmail.com>
2022-03-12 16:32:43 +0800

    Initial commit

---
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -0,0 +1,14 @@
+import { defineAsyncComponent } from 'vue'
+
+export const pagesComponents = {
+  // path: /
+  "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"G:/GitHub/JintaoYang-LearningPython/docs/.vuepress/.temp/pages/index.html.vue")),
+  // path: /config/cop.html
+  "v-f411d42e": defineAsyncComponent(() => import(/* webpackChunkName: "v-f411d42e" */"G:/GitHub/JintaoYang-LearningPython/docs/.vuepress/.temp/pages/config/cop.html.vue")),
+  // path: /config/front-matter.html
+  "v-79a80100": defineAsyncComponent(() => import(/* webpackChunkName: "v-79a80100" */"G:/GitHub/JintaoYang-LearningPython/docs/.vuepress/.temp/pages/config/front-matter.html.vue")),
+  // path: /config/palette.html
+  "v-040f4d5c": defineAsyncComponent(() => import(/* webpackChunkName: "v-040f4d5c" */"G:/GitHub/JintaoYang-LearningPython/docs/.vuepress/.temp/pages/config/palette.html.vue")),
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"G:/GitHub/JintaoYang-LearningPython/docs/.vuepress/.temp/pages/404.html.vue")),
+}
---
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -0,0 +1,16 @@
+export const siteData = {
+  "base": "/JintaoYang-LearningPython/",
+  "lang": "en-US",
+  "title": "Yang's Learning Python (unaccomplished)",
+  "description": "fun",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "./images/logo2.png"
+      }
+    ]
+  ],
+  "locales": {}
+}
